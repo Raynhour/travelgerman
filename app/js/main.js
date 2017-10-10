90d761699d11f802857aacb259ff59fc8207efd8
Raynhour <waqdet@gmail.com>
2017-10-10 15:55:17 +0300

    change

--- app/js/main.js
+++ app/js/main.js
@@ -5,6 +5,12 @@ $('document').ready(function(){
 	$('#inbox-list').click(function(){
 		$(".inbox__list").toggle();
 	});
+	$('#edit').click(function(){
+		$(".modal-edit").toggle();
+	});
+	$('.modal-edit__close, .modal-edit__close-text').click(function(){
+		$(".modal-edit").toggle();
+	});
 	$('.owl-carousel').owlCarousel({
 	    nav:true,
 	    navText: ["<img src='img/prev.png'>","<img src='img/next.png'>"],
@@ -24,5 +30,33 @@ $('document').ready(function(){
 	    	}
 	    }
 	});
+var swiper = new Swiper('.popup .swiper-container', {
+    pagination: '.popup .swiper-pagination',
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflow: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows : true
+    }
+});
+$('.popup .close_window, .overlay').click(function (){
+    $('.popup, .overlay').css({'opacity':'0', 'visibility':'hidden'});
+  });
+  
+  $(this).keydown(function(eventObject){
+    if (eventObject.which == 27){
+      $('.popup, .overlay').css({'opacity':'0', 'visibility':'hidden'});
+    }
+  });
+
+  $('a.open_window').click(function (e){
+    $('.popup, .overlay').css({'opacity':'1', 'visibility':'visible'});
+    // e.preventDefault();
+  });
 	
 });
